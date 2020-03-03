604defedc7662ee3b9b8f5029440bf4511ab8266
Oqqay <59747577+Oqqay@users.noreply.github.com>
2020-03-03 15:01:37 -0700

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,1 +1,15 @@
 import os
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+
+stemmer = LancasterStemmer()
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+
+with open("intents.json") as file:
+  data = json.load(file)
+
+print(data["intents"])
