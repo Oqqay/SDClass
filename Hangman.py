bb54788a66da7e47150ae6b6f5fe8297723b54a4
Oqqay <59747577+Oqqay@users.noreply.github.com>
2020-03-05 15:00:42 -0700

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -1,1 +1,11 @@
+import random
 
+def OneWord():
+  words = ['Caca','Doodoo','Kevin','Michael','Asa','Hello','Software','Engineering','Poop','Science','Computers','Hi','Python','Java','Momentum','Phone','Billy', 'Hacker', 'Forknite']
+  print(random.choice(words).upper())
+
+def TwoWords():
+  words = ['The Void', 'Software Development', 'Hey There', 'Chad Renning', 'Good Job', 'Hello Goodbye', 'Handsome Jack', 'Billy Bob', 'Kevin Ramiriez']
+
+
+#ef get_words():
